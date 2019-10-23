15afd8d433ba1b7746d073de92bcfa5345152b3b
Jeremiah Thuku <jeremiah.thuku@jubileekenya.com>
2019-10-23 12:16:38 +0300

    created app bar

--- lib/screens/movie_detail.dart
+++ lib/screens/movie_detail.dart
@@ -10,31 +10,51 @@ class MovieDetailScreen extends StatelessWidget{
 	Widget build(BuildContext context) {
 		return Scaffold(
 			backgroundColor: Colors.black,
-			appBar: AppBar(
-				elevation: 0.0,
-				leading: Icon(
-					Icons.menu,
+			appBar: PreferredSize(
+				preferredSize: Size(
+					MediaQuery.of(context).size.width,
+					56,
 				),
-				automaticallyImplyLeading: false,
-				centerTitle: true,
-				title: Text(
-					title,
-					style: TextStyle(
-						fontSize: 18.0,
-						fontWeight: FontWeight.w800,
+				child: AppBar(
+					elevation: 0.0,
+					leading: Icon(
+						Icons.menu,
 					),
-				),
-				actions: <Widget>[
-					InkWell(
-						onTap: (){},
-						child: Padding(
-							padding: EdgeInsets.symmetric(horizontal: 8.0),
-							child: Icon(
-								Icons.search,
+					automaticallyImplyLeading: false,
+					centerTitle: true,
+					title: Stack(
+						children: <Widget>[
+							Align(
+								alignment: Alignment.center,
+								child: Image.asset(
+									"assets/images/icon_movie_ticket.png",
+									height: 40,
+								),
+							),
+							Align(
+								alignment: Alignment.center,
+								child: Text(
+									title,
+									style: TextStyle(
+										fontSize: 20.0,
+										fontWeight: FontWeight.w800,
+									),
+								),
+							),
+						],
+					),
+					actions: <Widget>[
+						InkWell(
+							onTap: (){},
+							child: Padding(
+								padding: EdgeInsets.symmetric(horizontal: 8.0),
+								child: Icon(
+									Icons.search,
+								),
 							),
 						),
-					),
-				],
+					],
+				),
 			),
 			body: Container(
 				width: double.infinity,
