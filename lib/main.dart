a22b6c346f7aea73e1fe2e7402cc136e80d37738
Jeremiah Thuku <jeremiah.thuku@jubileekenya.com>
2019-10-23 11:18:35 +0300

    refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -17,55 +17,7 @@ class MyApp extends StatelessWidget {
 			debugShowCheckedModeBanner: false,
 			title: Constants.appName,
 			theme: Constants.darkTheme,
-			home: MyHomePage(title: Constants.appName),
+			home: MovieDetailScreen(title: Constants.appName),
 		);
-	}
-}
-
-class MyHomePage extends StatefulWidget {
-	MyHomePage({Key key, this.title}) : super(key: key);
-	
-	// This widget is the home page of your application. It is stateful, meaning
-	// that it has a State object (defined below) that contains fields that affect
-	// how it looks.
-	
-	// This class is the configuration for the state. It holds the values (in this
-	// case the title) provided by the parent (in this case the App widget) and
-	// used by the build method of the State. Fields in a Widget subclass are
-	// always marked "final".
-	
-	final String title;
-	
-	@override
-	_MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-	
-	@override
-	void initState() {
-		super.initState();
-		SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
-		SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-			statusBarColor: Constants.darkPrimary,
-			statusBarIconBrightness: Brightness.light,
-		));
-	}
-	int _counter = 0;
-	
-	void _incrementCounter() {
-		setState(() {
-			// This call to setState tells the Flutter framework that something has
-			// changed in this State, which causes it to rerun the build method below
-			// so that the display can reflect the updated values. If we changed
-			// _counter without calling setState(), then the build method would not be
-			// called again, and so nothing would appear to happen.
-			_counter++;
-		});
-	}
-	
-	@override
-	Widget build(BuildContext context) {
-		return MovieDetailScreen(title: Constants.appName);
 	}
 }
